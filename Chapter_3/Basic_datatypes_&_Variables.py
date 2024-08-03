e5df51b4d8e59ba63c2142aa6c8d817846ff4180
Muhammad Zuhair Zeb <zebzuhair71@gmail.com>
2024-08-03 15:03:26 +0500

    Create Basic_datatypes_&_Variables.py

---
+++ Chapter_3/Basic_datatypes_&_Variables.py
@@ -0,0 +1,21 @@
+# Example of using different data type in python
+#string
+name = "Pakistan"
+print(name)
+print(type(name))
+print(id(name))
+
+#integer(whole Number)
+age =20
+print(type(name))
+print(age)
+
+#Float(decimal)
+pi =3.14
+print(type(pi))
+print(pi)
+
+#bolean (True/False)
+bool_variable = True
+print(type(bool_variable))
+print(bool_variable)
